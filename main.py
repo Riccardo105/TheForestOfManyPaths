4e2fc877deba7357a066fbcc92cfa90cba36b05d
Riccardo Barone <riccardo.computerscience@gmail.com>
2024-09-30 21:03:36 +0100

    Validation function refactored, ongoing flow control manager implementation

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import time
 from Story import *
 import os
 
-def type_out_words(text, speed=0.1):
+
+def type_out_words(text, speed=0.02):
     """Prints text to the console as if it is being typed out, word by word."""
     # Split the text into lines and iterate through each line
     for line in text.strip().split('\n'):
@@ -15,43 +16,43 @@ def type_out_words(text, speed=0.1):
 
 class Narrator:
     def __init__(self):
+        # the user choices are stored for working out the story's final
         self.user_choice = []
-        self.story_steps = [introduction,         # introduction
-                            fork_in_the_road,     # Step 1
-                            overgrown_path,       # Step 2A
-                            foggy_path,           # Step 2B
-                            monument,             # Step 3A1
-                            living_grass,         # Step 3A2
-                            river_guardian,       # Step 3B1
-                            unending_mist,        # Step 3B2
-                            cabin_decision]       # Final Decision
+
+        # below are all the story steps sorted out by step/branch
+        self.introduction = [introduction,     # introduction
+                            fork_in_the_road]  # Step 1
+        self.branch_A = [overgrown_path,       # Step 2A
+                         monument,             # Step 3A1
+                         living_grass]         # Step 3A2
+        self.branch_B = [foggy_path,           # Step 2B
+                         river_guardian,       # Step 3B1
+                         unending_mist]        # Step 3B2
+        self.final_decision = cabin_decision   # Final Decision
         self.endings = [safe_haven,               # ending 1
                         shadow_within,            # ending 2
                         river_gift,               # ending 3
                         lost_in_mist]             # ending 4
+        # this starts the story
         self.flow_control_manager()
 
     # user input validation system
     def validate_input(self):
         user_choice = input()
-        if user_choice in ["A", "B"]:
-            self.user_choice.append(user_choice)
-            os.system('cls' if os.name == 'nt' else 'clear')  # if input is validated, console is cleared for next step
-            return True
+        while user_choice not in ["A", "B"]:
+            print('Invalid, please choose again:'"\n")
+            user_choice = input()
         else:
-            return False, "Invalid input, please choose again."
-
-    # the relevant user choices are appended for final ending calculation
-    def append_choice(self, user_choice):
-        self.user_choice.append(user_choice)
-        return self.user_choice
+            self.user_choice.append(user_choice)
 
     def print_ending(self):
         pass
 
     def flow_control_manager(self):
-        type_out_words(self.story_steps[0])
-        type_out_words(self.story_steps[1])
+        type_out_words(self.introduction[0])
+        type_out_words(self.introduction[1])
+        self.validate_input()
+
 
 
 narrator = Narrator()
